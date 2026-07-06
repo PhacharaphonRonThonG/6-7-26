ea0451dc1118d7cf655e672bd400c888a24d7b9e
PhacharaphonRonThonG <67020533@up.ac.th>
2026-07-06 14:29:50 +0700

    Add files via upload

---
+++ TestJava.java
@@ -0,0 +1,18 @@
+package testjava;
+import java.util.Scanner;
+public class TestJava {
+
+
+    public static void main(String[] args) {
+        double x;
+        int X,Y,Z;
+        X = 5;
+        Y = 10;
+        Z = 15;
+        System.out.println(X>Y||X<Z);
+        Scanner input = new Scanner(System.in);
+        x = input.nextDouble();
+        System.out.println ("Hello world!!! \n x ="+x);
+    }
+    
+}
